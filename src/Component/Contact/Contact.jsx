86eabee00251181dd60fa8430c3ed98af2f32766
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-12-18 01:25:30 +0600

    added file

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
 
 const Contact = () => {
     return (
         <div>
-            
+            <div>
+                
+            </div>
         </div>
     );
 };
