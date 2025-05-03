48be9d2c5ae3eda8dfaf4e4332574c14f6c202c2
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-05-03 13:06:08 +0600

    complete

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Blog = () => {
   return (
-    <div id="Blogs" className="bg-gradient-to-b from-gray-800 via-black to-black py-10">
+    <div id="Blogs" className="bg-gradient-to-b from-gray-800 via-black to-black py-10 px-3 md:px-6">
   <div className="max-w-6xl mx-auto">
     {/* Title */}
     <div className="text-center mb-12">
@@ -16,7 +16,7 @@ const Blog = () => {
     </div>
 
     {/* Blogs */}
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
       {blogData.slice(0, 3).map((blog, index) => (
         <div
           key={index}
